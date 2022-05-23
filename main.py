e7b7f511b6c7c37dc4125fa26a66ea96f0a07799
viswanathanTJ <30386026+viswanathanTJ@users.noreply.github.com>
2022-05-23 12:36:00 +0530

    Complete shop

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from supermarket import Shop
 def start_shop():
     my_shop = Shop()
     while(True):
-        # try:
-        command = input()
-        if command.lower() == "exit":
+        try:
+            command = input()
+            if command.lower() == "exit":
+                break
+            my_shop.execute(command)
+        except KeyboardInterrupt:
             break
-        my_shop.execute(command)
-        # except KeyboardInterrupt:
-        #     break
-        # except Exception as err:
-        #     print("Error:", err)
+        except Exception as err:
+            print("Error:", err)
 
 if __name__ == "__main__":
     start_shop()
